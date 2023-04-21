839aba945de8de3d11d9a251f26ece4e4143b8f0
nimra <nimi.tech.dev@gmail.com>
2023-04-21 11:08:11 -0700

    Add AuthController is Controller

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -2,5 +2,10 @@ class authController {
     constructor() {
         console.log("This is Constructor");
     }
+    static getName(req,res){
+        return res.json({
+            'message' : "This is getName"
+        });
+    }
 }
 module.exports = authController;
